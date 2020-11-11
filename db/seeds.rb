3cc4e4e3bddc5834930aab27ec2663ab06e594a2
Huy Vu	Nguyen <hv.ng@outlook.fr>
2020-11-11 14:47:05 +0100

    added

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,19 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+mac_do =  { name: "mac_do", address: "10 rue Jean Moulin", category: "belgian" }
+burger_king =  { name: "burger_king", address: "1 rue Camus", category: "italian" }
+five_guys =  { name: "five_guys", address: "1 place Ronde", category: "italian" }
+
+[ dishoom, pizza_east, mac_do, burger_king, five_guys ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
